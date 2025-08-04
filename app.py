ce4ace408a9ad377c43ea2a04e6aed013a560e7a
negin1380 <dehkhonegin@gmail.com>
2025-08-04 11:28:47 +0330

    change on model

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ EMBEDDING_MODEL_NAME = "BAAI/bge-m3"
 # api_key = "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"
 # apexion-ai/Nous-1-2B  طول کشید تا جواب داد 
 api_key = st.secrets["OPENROUTER_API_KEY"]                                                                                                                                                     
-TOP_K = 1  # Number of retrieved passages to include
+TOP_K = 3  # Number of retrieved passages to include
 
 # === Load models and data ===
 @st.cache_resource
