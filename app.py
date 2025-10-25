a65813136ca412e86c8ac6cb68e619d8c209c198
Neginde1380 <neginde1380@gmail.com>
2025-10-25 07:49:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def call_llm(query, context, max_retries=3):
     }
 
     data = {
-        "model": "google/gemma-3-4b-it:free",
+        "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
         #deepseek/deepseek-r1-0528-qwen3-8b:free
         "messages": [
             {
